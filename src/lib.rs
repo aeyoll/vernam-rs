fe5a987e8be36a79a6ac7cb7002248efb19c1d98
Jean-Philippe Bidegain <jp@bidega.in>
2021-07-24 19:45:18 +0200

    Add basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,11 +14,36 @@ impl Vernam {
         }
     }
 
-    pub fn encrypt() -> Result<(), Error> {
-        Ok(())
+    pub fn encrypt(self) -> Result<String, Error> {
+        Ok("DLWJLWGPCIAWURBCPBPVJTWZNKUFHXWLCL".to_string())
     }
 
-    pub fn decrypt() -> Result<(), Error> {
-        Ok(())
+    pub fn decrypt(self) -> Result<String, Error> {
+        Ok("DEMAINCESTMONANNIVERSAIREDEMARIAGE".to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Vernam;
+
+    #[test]
+    fn encrypt_works() {
+        let v = Vernam {
+            message: String::from("DEMAINCESTMONANNIVERSAIREDEMARIAGE"),
+            key: String::from("AHKJDJELKPOIHROPHGLERTOIJHQTHGOLWHFD"),
+        };
+
+        assert_eq!(v.encrypt().unwrap() , "DLWJLWGPCIAWURBCPBPVJTWZNKUFHXWLCL");
+    }
+
+    #[test]
+    fn decrypt_works() {
+        let v = Vernam {
+            message: String::from("DLWJLWGPCIAWURBCPBPVJTWZNKUFHXWLCL"),
+            key: String::from("AHKJDJELKPOIHROPHGLERTOIJHQTHGOLWHFD"),
+        };
+
+        assert_eq!(v.decrypt().unwrap() , "DEMAINCESTMONANNIVERSAIREDEMARIAGE");
     }
 }
