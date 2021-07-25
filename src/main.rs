1593939b28b015fc1bcd5d309c712d139090c6f5
Jean-Philippe Bidegain <jp@bidega.in>
2021-07-25 11:39:40 +0200

    Add a generic method to build a vernam instance

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::process;
 #[macro_use]
 extern crate clap;
 use clap::App;
+use clap::ArgMatches;
 
 mod lib;
 
@@ -12,40 +13,32 @@ extern crate failure;
 use failure::Error;
 use lib::Vernam;
 
+fn build_vernam(submatches: &ArgMatches) -> Result<Vernam, Error> {
+    let message = submatches.value_of("message").unwrap().to_string();
+    let key = submatches.value_of("key").unwrap().to_string();
+
+    if key.len() < message.len() {
+        bail!("Key length must be equal or greater than the message")
+    }
+
+    let v = Vernam { message, key };
+
+    Ok(v)
+}
+
 fn app() -> Result<(), Error> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
     match matches.subcommand() {
         ("encrypt", Some(submatches)) => {
-            let message = submatches.value_of("message").unwrap().to_string();
-            let key = submatches.value_of("key").unwrap().to_string();
-
-            if key.len() < message.len() {
-                bail!("Key length must be equal or greater than the message")
-            }
-
-            let v = Vernam {
-                message,
-                key,
-            };
-            let encrypted_message = v.encrypt().unwrap();
+            let vernam = build_vernam(submatches)?;
+            let encrypted_message = vernam.encrypt()?;
             println!("{}", encrypted_message);
         }
         ("decrypt", Some(submatches)) => {
-            let message = submatches.value_of("message").unwrap().to_string();
-            let key = submatches.value_of("key").unwrap().to_string();
-
-            if key.len() < message.len() {
-                bail!("Key length must be equal or greater than the message")
-            }
-
-            let v = Vernam {
-                message,
-                key,
-            };
-
-            let decrypted_message = v.decrypt().unwrap();
+            let vernam = build_vernam(submatches)?;
+            let decrypted_message = vernam.decrypt()?;
             println!("{}", decrypted_message);
         }
         _ => {
@@ -55,7 +48,6 @@ fn app() -> Result<(), Error> {
 
     Ok(())
 }
-
 
 fn main() {
     process::exit(match app() {
